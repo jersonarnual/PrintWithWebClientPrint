6cce4cafc7329dc2b577b04636215c417f972c55
Jerson Arnual Moreno Quevedo <jmoreno@corvatech.com>
2022-08-02 07:06:58 -0500

    Agregar archivos de proyecto.

---
+++ Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PrintWithWebClientPrint._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <div class="row">
+
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Imprimir" />
+
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using Neodynamic.SDK.Web;
+using System;
+using System.Web.UI;
+
+namespace PrintWithWebClientPrint
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            PrintFile file = null;
+            string filePath = @"~/FIle/Document.pdf";
+            string fileName = Guid.NewGuid().ToString("N");
+            file = new PrintFilePDF(MapPath(filePath), fileName);
+            ((PrintFilePDF)file).PrintRotation = PrintRotation.None;
+
+            ClientPrintJob cpj = new ClientPrintJob
+            {
+                PrintFile = file,
+                ClientPrinter = new DefaultPrinter()
+                //ClientPrinter =  new NetworkPrinter("192.168.1.1.8", 9100)
+            };
+            var pr = cpj.GetContent();
+            System.Web.HttpContext.Current.Response.ContentType = "application/octet-stream";
+            System.Web.HttpContext.Current.Response.BinaryWrite(cpj.GetContent());
+            System.Web.HttpContext.Current.Response.End();
+        }
+    }
+}
